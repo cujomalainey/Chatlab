f4508a6b667087485b8ec0831ec1ca3e8bef6872
Curtis <curtis@malainey.com>
2014-01-31 22:52:45 -0700

    Initial Commit

---
+++ Server/get_config.m
@@ -0,0 +1,12 @@
+function out = get_config(caseString)
+	out = [];
+	switch lower(caseString)
+		case 'db'
+			out.user = 'root';
+			out.host = 'localhost';
+			out.password = '';
+			out.db = 'matlab';
+		case 'process'
+			out.max_threads = 1;
+    end
+end
