ce1b33c100a22896ff0dff48f2e37489ae30a35a
Curtis <curtis@malainey.com>
2014-02-08 12:37:23 -0700

    encryption object methods complete

--- Client/encryptor.m
+++ Client/encryptor.m
@@ -2,31 +2,22 @@
 
 classdef encryptor
 
-% UI MEMBER HERE
-
-
-% UI CALLBACKS
-%%%%%%%%%%%%%%%%
-
-
-
-	properties (Hidden)
-		%AccountStatus = 'open';
-	end
-	% The following properties can be set only by class methods
 	properties (SetAccess = private)
 		key = [1 2 3; 2 5 3; 2 3 1];
     end
 	methods
-		function obj = UI_Controller() %#ok<STOUT>
+
+		function obj = UI_Controller()
 			%init key handler
 		end
+
 		function addKey(obj, key, chatId)
 			%BA.AccountBalance = BA.AccountBalance + amt;
 			%if BA.AccountBalance > 0
 			%	BA.AccountStatus = 'open';
 			%end
 		end
+
         function encryption = encrypt(obj, message, groupId)
 			ascii = double(message);
             %disp(groupId)
@@ -43,6 +34,7 @@ classdef encryptor
             end
             encryption = obj.key*response;
 		end % encrypt
+
         function message = decrypt(obj, encode, groupId)
             arr = inv(obj.key)*encode;
             columns = size(arr);
@@ -56,5 +48,37 @@ classdef encryptor
             end
             message = char(ascii);
         end %decrypt
+
+        function serial = serialize(obj, matrix)
+            columns = size(matrix);
+            columns = columns(2);
+            str = '';
+            for n = 1:columns
+                for i = 1:3 
+                    if ~(n == columns && i == 3)
+                        str = strcat(str, int2str(matrix(i, n)), ',');
+                    else
+                        str = strcat(str, int2str(matrix(i, n)));
+                    end
+                end
+            end
+            serial = str;
+        end
+
+        function unserial = unserialize(obj, str)
+            str = str2num(char(strsplit(str, ',')));
+            rows = size(str);
+            for n = 1:rows(1)
+                i = mod(n, 3);
+                if i == 0
+                    columns = (n - i)/3;
+                    i = 3;
+                else
+                    columns = (n - i)/3 + 1;
+                end
+                unserial(i,columns) = str(n, 1);
+            end
+        end
+
 	end % methods
 end % classdef
