f1ef1cf444acbeb22d7b79e283016ee7b841cd78
DavidSkrundz <david.skrundz@gmail.com>
2014-03-06 08:44:56 -0700

    Can now generate unique numbers for the duration of the program

---
+++ Server/Model/UniqueID.m
@@ -0,0 +1,24 @@
+classdef UniqueID < handle
+	
+	properties (GetAccess = 'public', SetAccess = 'private')
+		ID
+	end
+	
+	methods
+		function uid = UniqueID()
+			uid.ID = UniqueID.increment();
+		end
+	end
+	
+	methods (Static, Access = 'private')
+		function result = increment()
+			persistent stamp;
+			if isempty(stamp)
+				stamp = 0;
+			end
+			stamp = stamp + uint32(1);
+			result = stamp;
+		end
+	end
+	
+end
