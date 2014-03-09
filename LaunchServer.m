f2308a3538f0dc4427eac64f4816292974cc4e18
DavidSkrundz <david.skrundz@gmail.com>
2014-03-08 20:06:34 -0700

    The comments are tests for using the keygen process

--- LaunchServer.m
+++ LaunchServer.m
@@ -5,3 +5,40 @@ function [] = LaunchServer()
 	ClearPath();
 	main_server();
 end
+
+
+% x=[];
+% num=17;
+% for i=1:1:32
+% 	x(end+1)=mod(3^i, num);
+% 	hist(x,num-1);
+% 	pause(0.1);
+% end
+% 
+% x=[];
+% num=7;
+% for i=1:1:30
+% 	x(end+1)=mod(3^i, num);
+% 	hist(x,num-1);
+% 	pause(0.1);
+% end
+
+% fails = 0;
+% for i=1:1:1000
+% 	a = KeyManager();
+% 	b = KeyManager();
+% 	ka = a.buildKey(1);
+% 	kb = b.finishKey(ka, 1);
+% 	while strcmp(kb, '-1')
+% 		kb = b.finishKey(ka, 1);
+% 	end
+% 	a.addKey(kb, 1);
+% 	ka2 = a.getKey(1);
+% 	kb2 = b.getKey(1);
+% 	match = ka2 == kb2;
+% 	if match
+% 	else
+% 		fails = fails+1;
+% 	end
+% end
+% disp(fails);
